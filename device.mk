52d27f5feb787d3930e2d1492e20bb5d15006d63
bengris32 <bengris32@protonmail.ch>
2022-10-28 14:28:49 +0100

    RM6785: Shim misssing audio symbol
    * E vndksupport: Could not load /vendor/lib/hw/audio.primary.mt6785.so from default namespace: dlopen failed: cannot locate symbol "_ZN7android13TypeConverterINS_13DefaultTraitsI14audio_format_tEEE6mTableE" referenced by "/vendor/lib/hw/audio.primary.mt6785.so"....
    
    was removed in VNDK31, add it back.
    
    https://cs.android.com/android/_/android/platform/frameworks/av/+/dc6be0d5754e65ffa07fe8ef04ca4d28c2c0268b
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I9b90be74c40ec52624f84be24917b3392081a3bb
    Signed-off-by: Ansh <singhansh64321@gmail.com>

--- device.mk
+++ device.mk
@@ -57,6 +57,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.6.vendor:64
 
 PRODUCT_PACKAGES += \
+    libshim_audio \
     libshim_camera_metadata \
     libshim_mtkcam
 
@@ -321,7 +322,6 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_PACKAGES += \
-    libmedia_helper-v30 \
     libutils-v32 \
     libui-v32
 
