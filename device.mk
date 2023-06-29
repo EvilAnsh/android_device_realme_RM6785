84921f823862f4914528f71c120bbbe29203c401
Sarthak Roy <sarthakroy2002@gmail.com>
2023-06-29 13:30:36 +0000

    RM6785: device: Build the Aperture package
    Import and start building the Aperture package into the rom, and also update the default QR code component to Aperture's scanner component.
    
    Signed-off-by: Ansh <singhansh64321@gmail.com>

--- device.mk
+++ device.mk
@@ -51,6 +51,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    Aperture \
     android.hardware.camera.common@1.0.vendor:64 \
     android.hardware.camera.device@3.6.vendor:64 \
     android.hardware.camera.provider@2.6.vendor:64
