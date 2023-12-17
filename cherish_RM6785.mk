a26390669e9a5a3fac94b9cb21c77990f649c9d4
Ansh <singhansh64321@gmail.com>
2023-12-17 08:35:35 +0000

    RM6785: Cherish-ify
    Signed-off-by: Ansh <singhansh64321@gmail.com>

---
+++ cherish_RM6785.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
+# Inherit from device makefile
+$(call inherit-product, device/realme/RM6785/device.mk)
+
+# Inherit some common Sweet stuff.
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := cherish_RM6785
+PRODUCT_DEVICE := RM6785
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RM6785
+PRODUCT_MANUFACTURER := realme
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+# Target
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+CHERISH_BUILD_TYPE := OFFICIAL
+CHERISH_MAINTAINER := ꪖꪀ𝘴ꫝ|邪惡的
+TARGET_ENABLE_BLUR := true 
