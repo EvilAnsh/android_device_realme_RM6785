58cb18c5524c86f2c28353fc7de6e676d5af469b
Ansh <singhansh64321@gmail.com>
2023-12-21 04:45:49 +0000

    RM6785: Blaze-ify
    Signed-off-by: Ansh <singhansh64321@gmail.com>

---
+++ blaze_RM6785.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
+# Inherit from device makefile
+$(call inherit-product, device/realme/RM6785/device.mk)
+
+# Inherit some common ProjectBlaze stuff.
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := blaze_RM6785
+PRODUCT_DEVICE := RM6785
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RM6785
+PRODUCT_MANUFACTURER := realme
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+# Blaze
+BLAZE_BUILD_TYPE := OFFICIAL
+BLAZE_MAINTAINER := ꪖꪀ𝘴ꫝ|邪惡的
+TARGET_SUPPORTS_BLUR := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_INCLUDE_MATLOG := true
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+# Blaze: to build in jenkins
+curl -sL https://gist.githubusercontent.com/EvilAnsh/b7792b4d88b3d18ff7050e270d62bb6f/raw/62d6f3dc4370c31c88c001aa74a1925ba37a781f/Blaze.sh | bash
