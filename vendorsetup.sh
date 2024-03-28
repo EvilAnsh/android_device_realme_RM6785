39662c141ba07e3b0ddeba1c116d6b0c01d654ea
Ansh <singhansh64321@gmail.com>
2024-03-28 09:18:08 +0000

    RM6785: vendorsetup: Update the script
    - Switch to Samar's kernel
    - Trach hw/mediatek from LineageOS
    - Track sepolicy from LineageOS
    - Deprecate the usage of vendor/realme-firmware - it has been moved to vendor/realme/RM6785-common now and Track it from EvilAnsh
    
    Change-Id: I38c491863c8c396728032799898348e07723e3c1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,45 +1,37 @@
 echo 'Starting to clone stuffs needed for your device'
-echo 'Cloning RM6785 Vendor tree [1/7]'
+echo 'Cloning RM6785 Vendor tree [1/5]'
 # Vendor
 rm -rf vendor/realme/RM6785-common
 if [ ! -d "vendor/realme/RM6785-common" ]; then
     mkdir -p vendor/realme/RM6785-common
-    git clone https://github.com/realme-mt6785-devs/proprietary_vendor_realme_RM6785-common -b lineage-21 vendor/realme/RM6785-common
+    git clone https://github.com/EvilAnsh/proprietary_vendor_realme_RM6785-common -b lineage-21 vendor/realme/RM6785-common
 fi
 
-echo 'Cloning Vendor-Firmware[2/7]'
-# Vendor-Firmware
-rm -rf vendor/realme-firmware
-if [ ! -d "vendor/realme-firmware" ]; then
-    mkdir -p vendor/realme-firmware
-    git clone https://github.com/realme-mt6785-devs/proprietary_vendor_realme-firmware.git -b master vendor/realme-firmware
-fi
-
-echo 'Cloning Kernel tree [3/7]'
+echo 'Cloning Kernel tree [2/5]'
 # Kernel
 rm -rf kernel/realme/mt6785
 if [ ! -d "kernel/realme/mt6785" ]; then
     mkdir -p kernel/realme/mt6785
-    git clone https://github.com/PixelOS-Devices/android_kernel_realme_mt6785 -b fourteen kernel/realme/mt6785
+    git clone https://github.com/realme-mt6785-devs/android_kernel_realme_mt6785 -b lineage-21 kernel/realme/mt6785
 fi
 
-echo 'Cloning Mediatek SEpolicy_vndr [4/7]'
+echo 'Cloning Mediatek SEpolicy_vndr [3/5]'
 # SEpolicy
 rm -rf device/mediatek/sepolicy_vndr
 if [ ! -d "device/mediatek/sepolicy_vndr" ]; then
     mkdir -p device/mediatek/sepolicy_vndr
-    git clone https://github.com/realme-mt6785-devs/android_device_mediatek_sepolicy_vndr.git -b lineage-21 device/mediatek/sepolicy_vndr
+    git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-21 device/mediatek/sepolicy_vndr
 fi
 
-echo 'Cloning Hardware Mediatek [5/7]'
+echo 'Cloning Hardware Mediatek [4/5]'
 # Hardware
 rm -rf hardware/mediatek
 if [ ! -d "hardware/mediatek" ]; then
     mkdir -p hardware/mediatek
-    git clone https://github.com/realme-mt6785-devs/android_hardware_mediatek.git -b lineage-21 hardware/mediatek
+    git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-21 hardware/mediatek
 fi
 
-echo 'Cloning AOSP Clang 17.0.4 [7/7]' 
+echo 'Cloning AOSP Clang 17.0.4 [5/5]' 
 # AOSP Clang 17.0.4 
 rm -rf prebuilts/clang/host/linux-x86/r498229b 
 if [ ! -d "prebuilts/clang/host/linux-x86/r498229b" ]; then 
